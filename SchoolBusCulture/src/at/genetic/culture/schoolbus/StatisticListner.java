626e03c51ead3b673faf3d3ce2dc4148f239cb49
Natanael Arndt <arndtn@gmail.com>
2013-01-20 03:36:12 +0100

    add Statistic stuff

--- SchoolBusCulture/src/at/genetic/culture/schoolbus/StatisticListner.java
+++ SchoolBusCulture/src/at/genetic/culture/schoolbus/StatisticListner.java
@@ -12,6 +12,14 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.MinMaxCategoryRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import jenes.AlgorithmEventListener;
 import jenes.GenerationEventListener;
 import jenes.GeneticAlgorithm;
@@ -35,8 +43,7 @@ public class StatisticListner<T extends Chromosome> implements
 	}
 
 	private void openGui() {
-		JFrame frame = new JFrame("School Bus Culture");
-
+		JFrame frame = new JFrame("School Bus Culture – Map");
 		JPanel p = new JPanel();
 		// frame.setLayout(new GridLayout());
 		BoxLayout layout = new BoxLayout(p, BoxLayout.PAGE_AXIS);
@@ -53,6 +60,25 @@ public class StatisticListner<T extends Chromosome> implements
 		frame.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+		
+
+		JFrame frame2 = new JFrame("School Bus Culture – Stats");
+		JPanel p2 = new JPanel();
+		// frame.setLayout(new GridLayout());
+		BoxLayout layout2 = new BoxLayout(p, BoxLayout.PAGE_AXIS);
+		p.setLayout(layout2);
+
+		// Add some drawing panel and display the area map
+		// add another drawing panel and print live stats
+		// we need threads for this
+
+		p2.add(getStatPanel());
+
+		frame2.add(p2, BorderLayout.CENTER);
+
+		frame2.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+		frame2.pack();
+		frame2.setVisible(true);
 	}
 
 	@Override
@@ -115,7 +141,7 @@ public class StatisticListner<T extends Chromosome> implements
 		@Transient
 		public Dimension getPreferredSize() {
 			// TODO Auto-generated method stub
-			return new Dimension(w+40, h+40);
+			return new Dimension(w + 40, h + 40);
 			// return super.getPreferredSize();
 		}
 
@@ -125,7 +151,8 @@ public class StatisticListner<T extends Chromosome> implements
 
 			int[] max = getMax(stops);
 
-			System.out.println("Differenz der Punkte: " + max[0] + ", shift: " + max[1] + ", " + max[2]);
+			System.out.println("Differenz der Punkte: " + max[0] + ", shift: "
+					+ max[1] + ", " + max[2]);
 
 			Graphics2D g2 = (Graphics2D) g;
 
@@ -136,7 +163,6 @@ public class StatisticListner<T extends Chromosome> implements
 				int size = stops[i].numberOfPupils;
 				g2.fillOval(p.x, p.y, size, size);
 			}
-
 
 			p = project(stops[0], max[0], max[1], max[2]);
 			g2.setPaint(Color.RED);
@@ -160,22 +186,77 @@ public class StatisticListner<T extends Chromosome> implements
 			int max = (maxX > maxY ? maxX : maxY);
 			int diff = max - min;
 
-			int xoffset = minX + (maxX - minX)/2;
-			int yoffset = minY + (maxY - minY)/2;
-			
-			return new int[] {diff,xoffset,yoffset};
+			int xoffset = minX + (maxX - minX) / 2;
+			int yoffset = minY + (maxY - minY) / 2;
+
+			return new int[] { diff, xoffset, yoffset };
 		}
 
 		private Point project(BusStop stop, int diff, int xoffset, int yoffset) {
-			double scale = (double)w / (double)diff;
-			double shift = ((double)w / 2.0)+20;
+			double scale = (double) w / (double) diff;
+			double shift = ((double) w / 2.0) + 20;
 
-			int x = (int)(((double)stop.x - (double)xoffset) * scale + shift);
-			int y = (int)(((double)stop.y - (double)yoffset) * scale + shift);
+			int x = (int) (((double) stop.x - (double) xoffset) * scale + shift);
+			int y = (int) (((double) stop.y - (double) yoffset) * scale + shift);
 
 			Point p = new Point(x, y);
 			return p;
 		}
 	}
 
+	public ChartPanel getStatPanel() {
+
+		// create a dataset...
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1.0, "First", "Category 1");
+		dataset.addValue(4.0, "First", "Category 2");
+		dataset.addValue(3.0, "First", "Category 3");
+		dataset.addValue(5.0, "First", "Category 4");
+		dataset.addValue(5.0, "First", "Category 5");
+		dataset.addValue(7.0, "First", "Category 6");
+		dataset.addValue(7.0, "First", "Category 7");
+		dataset.addValue(8.0, "First", "Category 8");
+		dataset.addValue(5.0, "Second", "Category 1");
+		dataset.addValue(7.0, "Second", "Category 2");
+		dataset.addValue(6.0, "Second", "Category 3");
+		dataset.addValue(8.0, "Second", "Category 4");
+		dataset.addValue(4.0, "Second", "Category 5");
+		dataset.addValue(4.0, "Second", "Category 6");
+		dataset.addValue(2.0, "Second", "Category 7");
+		dataset.addValue(1.0, "Second", "Category 8");
+		dataset.addValue(4.0, "Third", "Category 1");
+		dataset.addValue(3.0, "Third", "Category 2");
+		dataset.addValue(2.0, "Third", "Category 3");
+		dataset.addValue(3.0, "Third", "Category 4");
+		dataset.addValue(6.0, "Third", "Category 5");
+		dataset.addValue(3.0, "Third", "Category 6");
+		dataset.addValue(4.0, "Third", "Category 7");
+		dataset.addValue(3.0, "Third", "Category 8");
+
+		// create the chart...
+		final JFreeChart chart = ChartFactory.createBarChart(
+				"Min/Max Category Plot", // chart title
+				"Category", // domain axis label
+				"Value", // range axis label
+				dataset, // data
+				PlotOrientation.VERTICAL, true, // include legend
+				true, // tooltips
+				false // urls
+				);
+
+		// NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+		// set the background color for the chart...
+		chart.setBackgroundPaint(Color.yellow);
+
+		// get a reference to the plot for further customisation...
+		final CategoryPlot plot = chart.getCategoryPlot();
+		plot.setRenderer(new MinMaxCategoryRenderer());
+		// OPTIONAL CUSTOMISATION COMPLETED.
+
+		// add the chart to a panel...
+		final ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+		return chartPanel;
+	}
 }
